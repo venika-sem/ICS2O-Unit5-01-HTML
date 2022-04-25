6e11b2feae6040b0f071b3cfc8eb6cf26e9611db
Venika Sem <99354917+venika-sem@users.noreply.github.com>
2022-04-25 17:05:45 +0000

    changed html

--- js/script.js
+++ js/script.js
@@ -23,3 +23,11 @@ if (navigator.serviceWorker) {
 function updateSliderValue(valueFromSlider) {
   document.getElementById("slider-value").innerHTML = valueFromSlider
 }
+
+/**
+ * This function displays the slider value.
+ */
+function myButtonClicked() {
+  document.getElementById("hello-world").innerHTML =
+    "<p>Value is: " + slider.value + "</p>"
+}
