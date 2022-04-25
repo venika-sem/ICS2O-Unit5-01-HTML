59b62002c62f2a0544c19082563a9773856c4bce
Venika Sem <99354917+venika-sem@users.noreply.github.com>
2022-04-25 16:40:38 +0000

    changed many

--- js/script.js
+++ js/script.js
@@ -9,21 +9,17 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-Unit6-01-PWA/sw.js", {
-    scope: "/ICS2O-Unit6-01-PWA/",
-  })
+  navigator.serviceWorker.register(
+    "/ICS2O-Unit5-01-HTML/sw.js",
+    {
+      scope: "/ICS2O-Unit5-01-HTML/",
+    }
+  )
 }
 
 /**
- * This function converts the temperature from fahrenheit to celsius.
+ * This function updates the slider value.
  */
-function convert() {
-  // input
-  const fahrenheitTemperature = parseFloat(document.getElementById('fahrenheit-temperature').value)
-
-   // process
-  const temperature = (fahrenheitTemperature - 32) * 5 / 9
-
-  // output
-  document.getElementById('temperature').innerHTML = '<p>The temperature in Celsius is: ' + temperature.toFixed(2) + ' °C</p>'
+function updateSliderValue(valueFromSlider) {
+  document.getElementById("slider-value").innerHTML = valueFromSlider
 }
